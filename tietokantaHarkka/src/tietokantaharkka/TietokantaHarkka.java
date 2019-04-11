e6d68aa8cd250f8b22a5a1733a624d4b72a371de
Jani Aakio <Aakio.Jani.P@student.uta.fi>
2019-04-11 13:39:09 +0100

    Testeri versio 0.1

--- tietokantaHarkka/src/tietokantaharkka/TietokantaHarkka.java
+++ tietokantaHarkka/src/tietokantaharkka/TietokantaHarkka.java
@@ -5,6 +5,8 @@
  */
 package tietokantaharkka;
 
+import view.testView;
+
 /**
  *
  * @author Cassu
@@ -15,7 +17,7 @@ public class TietokantaHarkka {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO Kutsutaan käyttöliittymä
+        testView.testUI();
     }
     
 }
