2b77db3c9199145d8787ef0900e2f6a3a5d48809
Nico <nikopetterialanko@gmail.com>
2019-04-21 17:30:32 +0300

    Add find-methods

--- tietokantaHarkka/src/tietokantaharkka/controllers/PrivateClientCont.java
+++ tietokantaHarkka/src/tietokantaharkka/controllers/PrivateClientCont.java
@@ -47,6 +47,7 @@ public class PrivateClientCont {
         }
     }
     
+    //IDllä
     public PrivateClient findPrivateClient(int nmbr, Connection con) throws SQLException{
         PrivateClient pC = null;
         PreparedStatement pStatement = null;
@@ -71,6 +72,77 @@ public class PrivateClientCont {
             }
         }
         return pC;
+    }
+    //Etunimellä
+    public ArrayList<PrivateClient> findPrivateClientByFName(String fName, Connection con) throws SQLException{
+        ArrayList<PrivateClient> pCAL = new ArrayList<PrivateClient>();
+	PrivateClient pC = null;
+        PreparedStatement pStatement = null;
+        ResultSet resultSet = null;
+	int rows = 0;
+        try {
+            con.setAutoCommit(false);
+	    pStatement = con.prepareStatement("SELECT COUNT(*) FROM henkilo WHERE etunimi = ?");
+	    pStatement.setString(1, fName);
+	    resultSet = pStatement.executeQuery();
+	    rows = resultSet.getInt(1);
+	    for (int i = 0; i < rows; i++) {
+	         pStatement = con.prepareStatement("SELECT asiakasnumero, etunimi, sukunimi, ROW_NUMBER() over (ORDER BY asiakasnumero) as rownum FROM henkilo WHERE etunimi = ? AND rownum = ?");
+	         pStatement.setString(1, fName);
+		 pStatement.setInt(2, i+1);
+		 pC = createPrivateClient(resultSet.getString(2), resultSet.getString(3), resultSet.getInt(1));
+		 pCAL.add(pC);
+	    }
+            con.commit();
+        }
+        catch(SQLException e) {
+            con.rollback(); 
+        }
+        finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (pStatement != null) {
+                pStatement.close();
+            }
+        }
+        return pCAL;
+    }
+
+    //Sukunimellä
+    public ArrayList<PrivateClient> findPrivateClientByLName(String lName, Connection con) throws SQLException{
+        ArrayList<PrivateClient> pCAL = new ArrayList<PrivateClient>();
+	PrivateClient pC = null;
+        PreparedStatement pStatement = null;
+        ResultSet resultSet = null;
+	int rows = 0;
+        try {
+            con.setAutoCommit(false);
+	    pStatement = con.prepareStatement("SELECT COUNT(*) FROM henkilo WHERE sukunimi = ?");
+	    pStatement.setString(1, lName);
+	    resultSet = pStatement.executeQuery();
+	    rows = resultSet.getInt(1);
+	    for (int i = 0; i < rows; i++) {
+	         pStatement = con.prepareStatement("SELECT asiakasnumero, etunimi, sukunimi, ROW_NUMBER() over (ORDER BY asiakasnumero) as rownum FROM henkilo WHERE sukunimi = ? AND rownum = ?");
+	         pStatement.setString(1, lName);
+		 pStatement.setInt(2, i+1);
+		 pC = createPrivateClient(resultSet.getString(2), resultSet.getString(3), resultSet.getInt(1));
+		 pCAL.add(pC);
+	    }
+            con.commit();
+        }
+        catch(SQLException e) {
+            con.rollback(); 
+        }
+        finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (pStatement != null) {
+                pStatement.close();
+            }
+        }
+        return pCAL;
     }
     
     public PrivateClient removePrivateClient(PrivateClient x, Connection con) throws SQLException {
