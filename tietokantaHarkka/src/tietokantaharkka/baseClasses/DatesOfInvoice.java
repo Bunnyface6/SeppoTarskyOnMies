66ef207668f9494d1970f095b5b6dab771ea4bd2
Jipsu <Jipsu@JipsuPC1>
2019-04-23 03:17:35 +0300

    Date puuttuu pW:stä

---
+++ tietokantaHarkka/src/tietokantaharkka/baseClasses/DatesOfInvoice.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tietokantaharkka.baseClasses;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jipsu
+ */
+public class DatesOfInvoice {
+    
+    Date date;
+    Date finalPayDate;
+
+    public DatesOfInvoice(Date date, Date finalPayDate) {
+        this.date = date;
+        this.finalPayDate = finalPayDate;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public Date getFinalPayDate() {
+        return finalPayDate;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setFinalPayDate(Date finalPayDate) {
+        this.finalPayDate = finalPayDate;
+    }
+}
